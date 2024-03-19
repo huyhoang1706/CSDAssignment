a820512de8d8729b0f85c9f5997d544b6f47e781
nngoc1824 <ngoctbhe186314@fpt.edu.vn>
2024-03-19 20:13:52 +0700

    add

--- App1/LinkList.java
+++ App1/LinkList.java
@@ -5,24 +5,28 @@ import shared.TaxPayer;
 
 public class LinkList {
     private static class Node {
-        TaxPayer infor;
+        TaxPayer taxPayer;
         Node next;
 
-        public Node(TaxPayer infor, Node next) {
-            this.infor = infor;
+        public Node() {
+        }
+
+        public Node(TaxPayer taxPayer, Node next) {
+            this.taxPayer = taxPayer;
             this.next = next;
         }
 
-        public Node(TaxPayer infor) {
-            this(infor, null);
+        public Node(TaxPayer taxPayer) {
+            this.taxPayer = taxPayer;
+            this.next = null;
         }
 
-        public TaxPayer getInfor() {
-            return infor;
+        public TaxPayer getTaxPayer() {
+            return taxPayer;
         }
 
-        public void setInfor(TaxPayer infor) {
-            this.infor = infor;
+        public void setTaxPayer(TaxPayer taxPayer) {
+            this.taxPayer = taxPayer;
         }
 
         public Node getNext() {
@@ -33,31 +37,118 @@ public class LinkList {
             this.next = next;
         }
     }
+
     private final Input input = new Input();
     private Node head, tail;
+
     // Public:
     public void addToEnd() {
         TaxPayer taxPayer = input.getTaxPayer();
+        Node newNode = new Node(taxPayer);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
     }
 
     public void addToBeginning() {
         TaxPayer taxPayer = input.getTaxPayer();
+        Node newNode = new Node(taxPayer);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
     }
 
-    public void searchByCode() {}
+    public Node searchByCode() {
+        String code = input.getString("Enter code: ");
+        Node currNode = head;
+        //
+        while (currNode != null) {
+            if (currNode.taxPayer.getCode().equals(code)) {
+                return currNode;
+            }
+            currNode = currNode.next;
+        }
+        return null;
+    }
 
-    public void deleteByCode() {}
+    public void deleteByCode()) {
+        String code = input.getString("Enter code: ");
+        Node currNode = head;
+        Node prevNode = null;
+        while (currNode != null) {
+            if (currNode.taxPayer.getCode().equals(code)) {
+                if (prevNode == null) {
+                    head = currNode.next;
+                } else {
+                    prevNode.next = currNode.next;
+                }
+                return;
+            }
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+    }
 
-    public void sortByCode() {}
+    public void sortByCode() {
+        Node currNode = head;
+        while (currNode != null) {
+            Node nextNode = currNode.next;
+            while (nextNode != null) {
+                if (currNode.taxPayer.getCode().compareTo(nextNode.taxPayer.getCode()) > 0) {
+                    TaxPayer temp = currNode.taxPayer;
+                    currNode.taxPayer = nextNode.taxPayer;
+                    nextNode.taxPayer = temp;
+                }
+                nextNode = nextNode.next;
+            }
+            currNode = currNode.next;
+        }
+    }
 
-    public void addAfterPosition() {}
+    public void addAfterPosition() {
+        int k = input.getInt("Enter position: ", 1, Integer.MAX_VALUE);
+        TaxPayer taxPayer = input.getTaxPayer();
+        Node newNode = new Node(taxPayer);
+        Node currNode = head;
+        for (int i = 1; i < k; i++) {
+            if (currNode == null) {
+                System.out.println("Position not found");
+                return;
+            }
+            currNode = currNode.next;
+        }
+        newNode.next = currNode.next;
+        currNode.next = newNode;
+    }
 
-    public void deleteAt() {}
+    public void deleteAt() {
+        int k = input.getInt("Enter position: ", 1, Integer.MAX_VALUE);
+        Node currNode = head;
+        Node prevNode = null;
+        for (int i = 1; i < k; i++) {
+            if (currNode == null) {
+                System.out.println("Position not found");
+                return;
+            }
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        if (prevNode == null) {
+            head = currNode.next;
+        } else {
+            prevNode.next = currNode.next;
+        }
+    }
 
-    public void addToEnd(TaxPayer taxPayer) {}
-
-    public boolean isEmpty() {
-        return head == null && tail == null;
+    public void addToEnd(TaxPayer taxPayer) {
     }
     // Private:
 }
